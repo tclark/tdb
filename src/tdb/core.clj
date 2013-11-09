d6cbaa31ece883a772d959d864c1a885076a702b
Tom Clark <tom.clark@op.ac.nz>
2013-11-10 08:32:00 +1300

    A few things:  1. Switched from using vectors for temporal records to sets;  2. Changed open and amend functions so that vtime values are passed in as args; 3. Wrote some tests.  Tests are cool

--- src/tdb/core.clj
+++ src/tdb/core.clj
@@ -3,9 +3,9 @@
 
 (defn uuid[] (str (java.util.UUID/randomUUID)))
 
-(defn open-record [id vtime]
+(defn open-record [id vtime ttime] 
   "Creates a new temporal record using the supplied id and valid timestamp"
-  [{:type :open :id id :vtime vtime :ttime (time/now)}])
+  #{{:type :open :id id :vtime vtime :ttime ttime}})
 
 (defn closed? [record]
   "Used to tell if a record is closed or not.  Returns
@@ -16,31 +16,32 @@
   "Returns true if the record is open for updates, false otherwise."
   (not (closed? record))) 
 
-(defn amend-record [record vtime type label value]
+(defn amend-record [record vtime ttime amedmenttype label value]
   "A general function to amend a record.  We don't typically call this function
    directly.  Instead we call special case methods like update-record or close-record.
    If you want to use a special type of amendment you could call this or write a
    function that calls amend-record to produce your admendment."
   (if (open? record)
-    (let [amendment { :type type :label label :value value :ttime (time/now) :vtime vtime}]
-      (vec (cons amendment record)))
+    (let [amendment { :type amedmenttype :label label :value value :ttime ttime :vtime vtime}]
+      (set (cons amendment record)))
     record))
 
-(defn update-record [record vtime label value]
+(defn update-record [record vtime ttime label value]
   "Produces an update amendment to a record that either
    adds a new label-value pair or overrides a previous one."
-  (amend-record record vtime :update label value))
+  (amend-record record vtime ttime :update label value))
 
-(defn close-record [record vtime reason]
+(defn close-record [record vtime ttime reason]
   "Closes a record by adding a close amendment.  A closed
    record cannot be amended further."
-    (amend-record record vtime :close :reason reason))
+    (amend-record record vtime ttime :close :reason reason))
 
 (defn temporal-filter [record qtime timetype]
   "Filters a record's items by removing all items
    with a timestamp after qtime.  Filters against 
    the timetype, which may be :vtime or :ttime."
-  (vec (filter  #(time/before? (timetype %) qtime) record)))
+  (let [mytime (time/plus qtime (time/seconds 1))]
+    (set (filter  #(time/before? (timetype %) mytime) record))))
 
 (defn valid-filter [record qtime]
   (temporal-filter record qtime :vtime))
@@ -51,5 +52,5 @@
 (defn get-latest-update [record label qtime timetype]
   "Given a temporal record, searches for the most recently added update
    relative to the query time and timetype (vtime or ttime) having the 
-   supplied label.  Returns the matching update or nil of no match is found""
+   supplied label.  Returns the matching update or nil of no match is found"
   (last (sort-by timetype (filter #(= (:label %) label) (temporal-filter record qtime timetype)))))
--- test/tdb/core_test.clj
+++ test/tdb/core_test.clj
@@ -1,7 +1,97 @@
 (ns tdb.core-test
   (:require [clojure.test :refer :all]
-            [tdb.core :refer :all]))
+            [tdb.core :refer :all]
+            [clj-time.core :as time]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(def times [(time/date-time 2013 6 15 14 00)
+                  (time/date-time 2013 6 16 11 14)
+                  (time/date-time 2013 7 1  22 5)
+                  ])
+(def new-open-record #{{:type :open :id 1 :vtime (get times 0) :ttime (get times 0)}})
+(def updated-record #{
+                 {:type :update :label :foo :value "bar" :vtime (get times 1) :ttime (get times 1)}
+                 {:type :open :id 1 :vtime (get times 0) :ttime (get times 0)}
+                  })
+(def bigger-record #{
+                 {:type :update :label :foo :value "baz" :vtime (get times 2) :ttime (get times 2)}
+                 {:type :update :label :foo :value "bar" :vtime (get times 1) :ttime (get times 1)}
+                 {:type :open :id 1 :vtime (get times 0) :ttime (get times 0)}
+                  })
+(def closed-record #{
+                 {:type :close :label :reason :value "test" :vtime (get times 1) :ttime (get times 1)}
+                 {:type :open :id 1 :vtime (get times 0) :ttime (get times 0)}
+                  })
+
+  
+(deftest test-open-record
+  (testing "Test open-record"
+  (is (= (open-record 1 (get times 0) (get times 0)) new-open-record) "Open record failed")))
+
+(deftest test-amend
+  (testing "Test amend-record"
+           (is (= (amend-record new-open-record (get times 1) (get times 1) :update :foo "bar")
+                  updated-record) "Update amendment failed") 
+             
+           (is (= (amend-record new-open-record (get times 1) (get times 1) :close :reason "test")
+                  closed-record) "Close amendment failed") 
+           (is (= (amend-record closed-record (get times 1) (get times 1) :update :foo "bar")
+                  closed-record) "Amend error: Performed amendment of a closed record")
+   )
+)
+
+(deftest test-closed
+  (testing "Test closed?"
+     (is (closed? closed-record) "Closed test failed")
+     (is (not (closed? new-open-record)) "Closed test failed on open record")
+     (is (not (closed? updated-record)) "Closed test failed on open record")
+  )
+)
+
+(deftest test-open
+  (testing "Test open?"
+     (is (open? new-open-record) "Open test failed")
+     (is (open? updated-record) "Open test failed on open record")
+     (is (not (open? closed-record)) "Open test failed o closed record")
+  )
+)
+
+(deftest test-update
+  (testing "Test update-record"
+    (is (= (update-record new-open-record (get times 1) (get times 1) :foo "bar")
+           updated-record) "Update failed")
+    (is (= (update-record closed-record (get times 1) (get times 1) :foo "bar")
+           closed-record) "Update error: Performed update of closed record")
+  )
+)
+
+(deftest test-close
+  (testing "Test close-record"
+     (is (= (close-record new-open-record (get times 1) (get times 1) "test")
+              closed-record) "Close record failed")
+     (is (= (close-record closed-record (get times 1) (get times 1) "test")
+              closed-record) "Close should be idempotent")
+  )
+ )
+
+(deftest test-filter
+  (testing "Test temporal filter"
+           (is (= (temporal-filter bigger-record (get times 2) :vtime) bigger-record) "Temporal filter failed with vtime")
+           (is (= (temporal-filter bigger-record (get times 1) :vtime) updated-record) "Temporal filter failed with vtime")
+           (is (= (temporal-filter bigger-record (get times 2) :ttime) bigger-record) "Temporal filter failed with ttime")
+           (is (= (temporal-filter bigger-record (get times 1) :ttime) updated-record) "Temporal filter failed with ttime")
+           (is (= (valid-filter bigger-record (get times 1)) updated-record) "Valid filter failed")
+           (is (= (transaction-filter bigger-record (get times 1)) updated-record) "Valid filter failed")
+  )
+)
+
+(deftest test-get-latest
+  (testing "Test get-latest-update"
+    (is (= (get-latest-update bigger-record :foo (get times 2) :vtime) 
+      {:type :update :label :foo :value "baz" :vtime (get times 2) :ttime (get times 2)})
+      "Get-latest failed")
+    (is (= (get-latest-update bigger-record :foo (get times 1) :vtime) 
+      {:type :update :label :foo :value "bar" :vtime (get times 1) :ttime (get times 1)})
+      "Get-latest failed")
+  )
+)
